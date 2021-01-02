3e1363393223e61b8b53d2805827c59b16c6de38
Nicolas Tanski <nicolas.tanski@gmail.com>
2021-01-01 23:36:29 -0300

    feat: server stream

--- cmd/server/client.go
+++ cmd/server/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/nicolastanski/grpc-go/pb"
@@ -18,7 +19,8 @@ func main() {
 	defer connection.Close()
 
 	client := pb.NewUserServiceClient(connection)
-	AddUser(client)
+	//AddUser(client)
+	AddUserVerbose(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -34,4 +36,28 @@ func AddUser(client pb.UserServiceClient) {
 	}
 
 	fmt.Println(res)
+}
+
+func AddUserVerbose(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "0",
+		Name:  "João",
+		Email: "j@j.com",
+	}
+
+	responseStream, err := client.AddUserVerbose(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	for {
+		stream, err := responseStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Could not receive the msg: %v", err)
+		}
+		fmt.Println("Status:", stream.Status, " - ", stream.GetUser())
+	}
 }
--- services/user.go
+++ services/user.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/nicolastanski/grpc-go/pb"
 )
@@ -24,4 +25,44 @@ func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error)
 		Name:  req.GetName(),
 		Email: req.GetEmail(),
 	}, nil
+}
+
+func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVerboseServer) error {
+	stream.Send(&pb.UserResultStream{
+		Status: "Init",
+		User:   &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Inserting",
+		User:   &pb.User{},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "User has been inserted",
+		User: &pb.User{
+			Id:    "123",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Completed",
+		User: &pb.User{
+			Id:    "123",
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		},
+	})
+
+	time.Sleep(time.Second * 3)
+
+	return nil
 }
